920601978549e252f7d51c81171270faf223d828
StatsWiz <sfrancov@users.noreply.github.com>
2020-09-28 15:28:32 -0500

    Completo

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -73,4 +73,7 @@ public class Animal {
     public String toString(){
         return "Mi nombre es " +nombre+", tengo una edad de " +edad+", habito en "+habitat+"y mi genero es "+genero+", la zona en la que me ubico es "+zona+", en el #zoo";
     }
+    public static String totalPorTipo(){
+        return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + "Aves: " + Ave.cantidadAves() + "\n" + "Reptiles: " + Reptil.cantidadReptiles() + "\n" + "Peces: "+ Pez.cantidadPeces()+ "\n" + "Anfibios: " + Anfibio.cantidadAnfibios();
+    }
 }
